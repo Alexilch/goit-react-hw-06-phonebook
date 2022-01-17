0cf727ef392cd8d4a0d5903f2fe67b0042f7f2f7
Alexander Ilchenko <ilchenko.alexander.n@gmail.com>
2022-01-17 17:49:04 +0200

    clear commented

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,15 +1,4 @@
-// import { nanoid } from 'nanoid';
-// import types from './types';
 import { createAction } from '@reduxjs/toolkit';
-
-// const addContact = (name, number) => ({
-//     type:types.ADD,
-//     payload:{
-//         id: nanoid(5),
-//         name,
-//         number,
-//       }
-// });
 
 export const addContact = createAction('contacts/add');
 export const deleteContact = createAction('contacts/delete');
