d189274bce5fef9056850a9727d41e7874ef8be2
Alexander Ilchenko <ilchenko.alexander.n@gmail.com>
2022-01-20 13:06:05 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,12 +1,26 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContacts = state => state.contacts.items;
 export const getFilter = state => state.contacts.filter;
+//with memo
+export const filteredContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
 
-export const filteredContacts = state => {
-  const contacts = getContacts(state);
-  const filter = getFilter(state);
-  const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter),
+    );
+  },
+);
 
-  return contacts.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter),
-  );
-};
+//without memo
+// export const filteredContacts = state => {
+//   const contacts = getContacts(state);
+//   const filter = getFilter(state);
+//   const normalizedFilter = filter.toLowerCase();
+
+//   return contacts.filter(({ name }) =>
+//     name.toLowerCase().includes(normalizedFilter),
+//   );
+// };
